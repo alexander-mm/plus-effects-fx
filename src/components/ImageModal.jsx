b2054f488acf7f86a2a29232b6e1c0c7858935c7
alexander-mm <alexanderwolftrader@gmail.com>
2024-01-23 12:21:19 -0500

    full-screen-fixed

--- src/components/ImageModal.jsx
+++ src/components/ImageModal.jsx
@@ -1,22 +1,43 @@
 import closeButton from '../assets/icons/red-close-button.svg';
+import { useRef } from 'react';
 
 // eslint-disable-next-line react/prop-types
 const ImageModal = ({ imageUrl, closeModal }) => {
+    const imgRef = useRef(null);
 
     return (
-        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-[.88] z-50">
-            <div className="flex flex-col items-end bg-black p-4 rounded-lg shadow-lg">
-                <button onClick={closeModal} className="w-[60px] my-1 text-white p-[14px] rounded-md">
-                    <div className='border rounded-full p-[3px]'>
-                        <img className="w-full cursor-pointer" src={closeButton} alt="cerrar form" />
-                    </div>
-                </button>
-                <div
-                    ref={imageUrl}
-                    className="cursor-pointer"
-                >
-                    <img src={imageUrl} className="w-full rounded-xl" alt="Large Image" />
+        <div
+            className="p-20 flex-col z-[1000] fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-75"
+            onClick={closeModal}
+        >
+            <button
+                onClick={closeModal}
+                className="self-end w-[60px] mt-[em] text-white p-[14px] rounded-md"
+            >
+                <div className='border rounded-full p-[3px]'>
+                    <img className="w-full cursor-pointer" src={closeButton} alt="cerrar form" />
                 </div>
+            </button>
+            <div
+                style={{
+                    position: 'relative',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: 'auto',
+                    zIndex: -1,
+                }}
+                ref={imgRef}
+            >
+                <img
+                    src={imageUrl}
+                    alt="modal image"
+                    className="w-full h-auto"
+                    style={{
+                        objectFit: 'contain',
+                        maxHeight: '80vh',
+                    }}
+                />
             </div>
         </div>
     );
