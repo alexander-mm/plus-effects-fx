9460ae3de49733f2f48c2bddfefad04b033c8bdc
alexander-mm <alexanderwolftrader@gmail.com>
2024-01-24 16:05:56 -0500

    PayPhone

--- public/respuesta.php
+++ public/respuesta.php
@@ -19,23 +19,15 @@
     curl_setopt($curl, CURLOPT_POST, 1);
     curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
     curl_setopt_array($curl, array(
-        CURLOPT_HTTPHEADER => array(
-            "Authorization: Bearer iYabwnS13BDt7TUBQckHRY3KQEPo7ozjPR0keZBNF7w-7dJFlB1Yixoc8o8OAMOKSyX7McAHQXc4LGmAVmMxXZtnq_B28gouU8XqujvIIF93CzwcV5QWm53UTnIt8pG6FiRfUWkDVCUztWYS652tfK5JKH3tXJNT0-fy6A3gPCEoEp3rnD8CjOvS4W2iixFppec4tgYYw93LF-LKoj5SUyP79tztu_AFojpu98vBZmqUXp8IiVEtFyHgf9wxTguoekdlXoacdQUnYEcaxWuQe_65Ykg6OgmZ1Bc19-5Z4xA6m7TikMpp_xtAJX4oWEuOsSuW7w",
-            "Content-Type: application/json"
-        ),
-        CURLOPT_RETURNTRANSFER => 1
+    CURLOPT_HTTPHEADER => array(
+    "Authorization: Bearer hNorVxVvpjSSF13D1XMOlbXs0rJG6Dpb7lYHlY76nlpc5NmzdwSyyDHKkg79D2bvvFS543ezrhL55bTzgAk8NSQu9cfIu235PIRdOo3kqRy5sbCQ7hoKn47cuUbq5LffPb1NAsh1t-EK-frdWcehkDl1dkG1_ulLZ6FuuXMdJA-b6b777qoJTaeaHymWcfaFQ4dCq9Taa6zhUHmLkyu0NW-tUuvN1uNJKvWocfuI_V7n1HHeUf2CkNhjW5lpaSAUILyJLGBWL2V4IcT3FGRy706wXUKTlBuFhd6ytlwrYcS4LeSqS8FABeshsVOvVydnDLSINg", "Content-Type:application/json"),
     ));
-    
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
     $result = curl_exec($curl);
-    $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-    if ($httpCode == 200) {
-        echo $result;
-    } else {
-        echo "Error en la confirmación. Código HTTP: " . $httpCode;
-    }
-
     curl_close($curl);
+    echo $result;
+
     fwrite($file, "Fin de llamada" . PHP_EOL);
     fwrite($file, $result . PHP_EOL);
-?>
+
+    ?>
